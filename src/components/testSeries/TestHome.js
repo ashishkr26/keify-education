8697a7cb08b4fd8c7430a70bfae45ba020ca963e
ashishkr26 <ashish.dypcoe@gmail.com>
2024-12-24 22:53:19 +0530

    test-series first page

--- src/components/testSeries/TestHome.js
+++ src/components/testSeries/TestHome.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const TestHome = () => {
+  const testList = ["Test-01", "Test-02"];
   return (
-    <div>
-      Welcome to the test
+    <div className="flex flex-row font-poppins ">
+      <div className="w-1/2 m-4 p-4 text-2xl font-bold text-blue-500">
+        GATE- FULL LENGTH TESTS
+      </div>
+      <div className="w-1/2 border border-slate-950 m-4 h-80 shadow-inner">
+        {testList.map((item) => (
+          <div className="flex m-2  p-2 justify-between">
+            <div className="bg-green-100 flex justify-center items-center p-2 rounded-md ">
+              FULL LENGTH {item}
+            </div>
+            <div>
+              <button className="px-2 py-1 bg-blue-400 rounded-md">
+                Start
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TestHome
+export default TestHome;
